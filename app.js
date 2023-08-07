15eabd057f0f6e72049872ce0a23eab38f3b425e
Melvn <mgrobinson35@gmail.com>
2023-08-07 19:45:45 -0400

    Started on app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('myBtn');
+const modal = document.querySelector('myModal');
+
+const openModal = function() {
+    modal.classList.remove('hidden')
+    
+};
+myBtn.addEventListener('click',openModal() 
+    
+);
